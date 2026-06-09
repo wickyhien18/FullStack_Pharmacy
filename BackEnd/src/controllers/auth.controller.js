f1c54fc2ac5dd0402fa237dba40bb703f12ff094
wickyhien18 <giaphien1008@gmail.com>
2026-06-09 10:31:53 +0700

    update auth.controller.js

--- BackEnd/src/controllers/auth.controller.js
+++ BackEnd/src/controllers/auth.controller.js
@@ -12,10 +12,7 @@ const cookieOptions = {
 
 export const register = async (req, res) => {
   try {
-    const { accessToken, refreshToken, user } = await authService.register(
-      req.body,
-    );
-    res.cookie(REFRESH_TOKEN_COOKIE, refreshToken, cookieOptions);
+    const user = await authService.register(req.body);
     return sendSuccess(res, user, "User registered successfully", 201);
   } catch (error) {
     sendError(res, error.message, error.status || 500);
