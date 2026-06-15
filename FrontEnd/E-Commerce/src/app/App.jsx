5957710f78f02c54630471914d2272816e9b5ed1
wickyhien18 <giaphien1008@gmail.com>
2026-06-15 18:56:19 +0700

    update App.jsx and useAuth.js

--- FrontEnd/E-Commerce/src/app/App.jsx
+++ FrontEnd/E-Commerce/src/app/App.jsx
@@ -1,8 +1,7 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 function App() {
+  //adasd
   return <RouterProvider router={router} />;
 }
-export {
-  App as default
-};
+export { App as default };
