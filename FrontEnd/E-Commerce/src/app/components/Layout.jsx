28e56ec7afa3d1187ddd77d01bff77a84d660cb8
wickyhien18 <giaphien1008@gmail.com>
2026-06-16 09:50:11 +0700

    update Layout.jsx

--- FrontEnd/E-Commerce/src/app/components/Layout.jsx
+++ FrontEnd/E-Commerce/src/app/components/Layout.jsx
@@ -3,16 +3,19 @@ import { CartProvider } from "../context/CartContext";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 function Layout() {
-  return <CartProvider>
-      <div className="min-h-screen flex flex-col" style={{ backgroundColor: "#f4f5f7" }}>
+  return (
+    <CartProvider>
+      <div
+        className="min-h-screen flex flex-col"
+        style={{ backgroundColor: "#f4f5f7" }}
+      >
         <Header />
         <main className="flex-1">
           <Outlet />
         </main>
         <Footer />
       </div>
-    </CartProvider>;
+    </CartProvider>
+  );
 }
-export {
-  Layout as default
-};
+export { Layout as default };
