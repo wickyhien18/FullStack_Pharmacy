5a7a29394dfc97b10b6e8863fcd1a91989a384cc
wickyhien18 <giaphien1008@gmail.com>
2026-06-19 12:08:08 +0700

    update admin.routes.js

--- BackEnd/src/routes/admin.routes.js
+++ BackEnd/src/routes/admin.routes.js
@@ -21,8 +21,8 @@ router.patch("/orders/:orderId/status", adminController.updateOrderStatus);
 // Users
 router.get("/users", adminController.getAllUsers);
 router.patch("/users/:userId/status", adminController.updateUserStatus);
-router.patch("/users/:userId/role", updateUserRole);
-router.get("/roles", getRoles);
+router.patch("/users/:userId/role", adminController.updateUserRole);
+router.get("/roles", adminController.getRoles);
 
 // Medicines
 router.get("/medicines", adminController.getAllMedicines);
