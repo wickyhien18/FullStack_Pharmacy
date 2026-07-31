8dc1092f4a5783566ef63c1d010d51ff2cf2b7c1
wickyhien18 <giaphien1008@gmail.com>
2026-07-31 15:10:43 +0700

    update socket.js

--- BackEnd/src/config/socket.js
+++ BackEnd/src/config/socket.js
@@ -1,9 +1,12 @@
+// Store instance of Socket.IO
 let ioInstance = null;
 
+// A function that stores the Socket.IO instance into a global variable within the module.
 export const setIO = (io) => {
   ioInstance = io;
 };
 
+// A function that retrieves the Socket.IO instance for use in other services or controllers
 export const getIO = () => {
   if (!ioInstance) throw new Error("Socket.io chưa được khởi tạo");
   return ioInstance;
